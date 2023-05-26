19ea1318744cf326d8a5ef19c81ca365fb8211b2
Kashif Soofi <kashif.soofi+git@gmail.com>
2023-05-26 22:18:07 +0100

    Add demo project for REST API using ASP.NET Core 7 with Postgres

---
+++ restapi-with-asp.net-core-7-and-postgres/Movies.Api/Store/Postgres/Sql/Update.sql
@@ -0,0 +1,8 @@
+﻿UPDATE movies
+SET
+    title = @title,
+    director = @director,
+    release_date = @release_date,
+    ticket_price = @ticket_price,
+    updated_at = @updated_at
+WHERE id = @id
