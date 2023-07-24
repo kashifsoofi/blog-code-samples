5c5b30a733b3e98235b336f32b51faf520f2629f
Kashif Soofi <kashif.soofi+git@gmail.com>
2023-07-24 19:05:54 +0100

    Copy project

---
+++ integration-test-myql/Movies.Api/Domain/Movie.cs
@@ -0,0 +1,43 @@
+﻿namespace Movies.Api.Domain;
+
+public class Movie
+{
+    public Guid Id { get; }
+    public string Title { get; }
+    public string Director { get; }
+    public decimal TicketPrice { get; }
+    public DateTime ReleaseDate { get; }
+    public DateTime CreatedAt { get; }
+    public DateTime UpdatedAt { get; }
+
+    public Movie(
+        Guid id,
+        string title,
+        string director,
+        decimal ticketPrice,
+        DateTime releaseDate,
+        DateTime createdAt,
+        DateTime updatedAt
+        )
+    {
+        Id = id;
+        Title = title;
+        Director = director;
+        TicketPrice = ticketPrice;
+        ReleaseDate = releaseDate;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
+    }
+
+    public Movie(Store.Movie storeMovie)
+    {
+        Id = storeMovie.Id;
+        Title = storeMovie.Title;
+        Director = storeMovie.Director;
+        TicketPrice = storeMovie.TicketPrice;
+        ReleaseDate = storeMovie.ReleaseDate;
+        CreatedAt = storeMovie.CreatedAt;
+        UpdatedAt = storeMovie.UpdatedAt;
+    }
+}
+
