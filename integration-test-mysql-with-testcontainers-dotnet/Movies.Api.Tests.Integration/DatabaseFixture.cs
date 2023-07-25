fa63ac5e71586dacb466eab10b141c450ca5d30a
Kashif Soofi <kashif.soofi+git@gmail.com>
2023-07-25 10:15:52 +0100

    Copied sample project as base

---
+++ integration-test-mysql-with-testcontainers-dotnet/Movies.Api.Tests.Integration/DatabaseFixture.cs
@@ -0,0 +1,18 @@
+﻿namespace Movies.Api.Tests.Integration;
+
+public class DatabaseFixture : IAsyncLifetime
+{
+    public string ConnectionString { get; private set; } = default!;
+
+    public async Task InitializeAsync()
+    {
+        this.ConnectionString = "server=localhost;database=Movies;uid=root;password=Password123;SslMode=None;";
+        await Task.CompletedTask;
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Task.CompletedTask;
+    }
+}
+
---
+++ integration-test-mysql-with-testcontainers-dotnet/Movies.Api/Program.cs
@@ -0,0 +1,34 @@
+﻿using Movies.Api.Store;
+using Movies.Api.Store.InMemory;
+using Movies.Api.Store.MySql;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHealthChecks();
+
+// Add services to the container.
+// builder.Services.AddSingleton<IMoviesStore, InMemoryMoviesStore>();
+builder.Services.AddScoped<IMoviesStore, MySqlMoviesStore>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapHealthChecks("/healthz");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
