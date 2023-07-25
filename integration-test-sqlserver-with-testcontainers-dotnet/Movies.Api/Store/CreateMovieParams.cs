3ab85dd32efd1e430c37080acdfc91a5cfe6fa14
Kashif Soofi <kashif.soofi+git@gmail.com>
2023-07-25 22:50:22 +0100

    Copied sample project for integration-test-sqlserver-with-testcontainers-dotnet

---
+++ integration-test-sqlserver-with-testcontainers-dotnet/Movies.Api/Store/CreateMovieParams.cs
@@ -0,0 +1,26 @@
+﻿namespace Movies.Api.Store;
+
+public class CreateMovieParams
+{
+    public Guid Id { get; }
+    public string Title { get; }
+    public string Director { get; }
+    public decimal TicketPrice { get; }
+    public DateTimeOffset ReleaseDate { get; }
+
+    public CreateMovieParams(
+        Guid id,
+        string title,
+        string director,
+        decimal ticketPrice,
+        DateTimeOffset releaseDate
+        )
+    {
+        Id = id;
+        Title = title;
+        Director = director;
+        TicketPrice = ticketPrice;
+        ReleaseDate = releaseDate;
+    }
+}
+
---
+++ integration-test-sqlserver-with-testcontainers-dotnet/Movies.Api/Store/SqlServer/Sql/Delete.sql
@@ -0,0 +1,3 @@
+DELETE
+FROM Movies
+WHERE Id = @Id
